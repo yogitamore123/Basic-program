1a2abef2b9c5aa15e6a49dcb15a1157d26113b4e
yogitamore123 <moreyogita120@gmail.com>
2024-08-21 12:08:32 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,12 @@
+class string {
+    public static void main(String[] args) {
+        String s1="Sachin";
+        String s2="Sachin";
+        String s3=new String("Sachin");
+        String s4="Saurav";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+    }
+    
+}
