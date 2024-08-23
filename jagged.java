c524406c42de7f0f1203464dbacfd575cb5f9d8d
yogitamore123 <moreyogita120@gmail.com>
2024-08-23 14:48:33 +0530

    Add files via upload

---
+++ jagged.java
@@ -0,0 +1,17 @@
+class jagged {
+    public static void main(String[] args) {
+        int arr[][]={{1,5,7},{2,1},{5,10,11,15}};
+     
+        
+            for(int i=0;i<arr.length;i++)
+                {
+                    for(int j=0;j<arr[i].length;j++){
+                        System.out.print(arr[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+
+        }
+    }
+    
+
