72e4359ec5299385c25a0aafa132b9c698223770
yogitamore123 <moreyogita120@gmail.com>
2024-09-13 13:19:32 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,14 @@
+public class Count {
+    public static void main(String[] args) {
+        String str="Automation";
+        int vowels=0,consonants=0;
+        for(char c:str.toCharArray()){
+            if("aeiouAEIOU".indexOf(c)!=-1){
+                vowels++;
+            }else if(Character.isLetter(c)){
+                consonants++;
+            }
+        }
+        System.out.println("Vowels:"+vowels+"Consonants:"+consonants);
+    }
+}
